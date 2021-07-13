8744d2f9afc16cfda96ea6925946ab0ce400a1cd
bmxitalia <tommasocarraro96@gmail.com>
2021-07-13 11:56:35 +0200

    debug loss function MNIST example

--- examples/mnist_single_digits_addition.py
+++ examples/mnist_single_digits_addition.py
@@ -151,9 +151,12 @@ def main():
 
         # we print metrics every epoch of training
         # | Train Acc %.3f | Test Acc %.3f compute_accuracy(train_loader), compute_accuracy(test_loader)
+        '''
         logger.info(" epoch %d | loss %.4f | Train Sat %.3f | Test Sat %.3f | Train Acc %.3f | Test Acc %.3f ",
                     epoch, train_loss, compute_sat_level(train_loader), compute_sat_level(test_loader),
-                    compute_accuracy(train_loader), compute_accuracy(test_loader))
+                    compute_accuracy(train_loader), compute_accuracy(test_loader))'''
+        logger.info(" epoch %d | loss %.4f ",
+                    epoch, train_loss)
 
 
 
